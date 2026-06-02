c3433473071e898f53acc0ad8688090c1f32b557
xie_weiping <xie@xie-weiping>
2026-06-02 10:18:19 +0800

    fix: 强制所有子项目 compileSdkVersion 为 34（修复 package_info_plus 等插件 SDK 版本不兼容）

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -3,6 +3,21 @@ allprojects {
         google()
         mavenCentral()
     }
+    configurations.all {
+        resolutionStrategy {
+            force("com.android.tools:desugar_jdk_libs:2.1.4")
+        }
+    }
+}
+
+subprojects {
+    plugins.withId("com.android.library") {
+        extensions.configure<com.android.build.gradle.LibraryExtension>("android") {
+            if (compileSdkVersion < 34) {
+                compileSdkVersion = 34
+            }
+        }
+    }
 }
 
 tasks.register<Delete>("clean") {
